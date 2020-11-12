04fd6a573f847c1c63ad2c1eb7a0acbb5fbcb267
Drew C <drewcav96@gmail.com>
2020-11-12 17:00:09 -0500

    User accounts do not need confirmation

--- DinderWS/Startup.cs
+++ DinderWS/Startup.cs
@@ -26,7 +26,9 @@ namespace DinderWS {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
