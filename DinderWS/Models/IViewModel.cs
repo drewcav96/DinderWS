9ef693b570b95977dc9dc9d8e2b81f6b08dbc797
Drew C <drewcav96@gmail.com>
2020-11-12 17:00:54 -0500

    Create interface for all view models

---
+++ DinderWS/Models/IViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DinderWS.Models {
+    /// <summary>
+    /// Interface for the view model.
+    /// </summary>
+    public interface IViewModel {
+        /// <summary>
+        /// The Title or main header for this view.
+        /// </summary>
+        string ViewTitle { get; }
+        /// <summary>
+        /// The error message from the server, if any.
+        /// </summary>
+        string Error { get; }
+    }
+}
