3dea888561ea89bce9c0b0713041ffda90367d14
Drew C <drewcav96@gmail.com>
2020-11-12 17:35:21 -0500

    Updated SQL connection string

--- DinderWS/Data/ApplicationDbContext.cs
+++ DinderWS/Data/ApplicationDbContext.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DinderWS.Data {
-    public class ApplicationDbContext : IdentityDbContext {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) {
+    public sealed class ApplicationDbContext : IdentityDbContext {
+        private readonly ILogger<ApplicationDbContext> _log;
+
+        public ApplicationDbContext(ILogger<ApplicationDbContext> logger,
+                DbContextOptions<ApplicationDbContext> options)
+                    : base(options) {
+            _log = logger;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+            _log.LogInformation($"Application Database Context connected to {Database.ProviderName}.");
         }
     }
 }
