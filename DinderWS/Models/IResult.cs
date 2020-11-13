7c6c15641252a0cdfe62a7be7c1fe05fd08e7f6d
Drew C <drewcav96@gmail.com>
2020-11-12 22:14:18 -0500

    Created the object result interface & IdentityResult

---
+++ DinderWS/Models/IResult.cs
@@ -0,0 +1,19 @@
+﻿namespace DinderWS.Identity {
+    /// <summary>
+    /// Interface for the Result.
+    /// </summary>
+    public interface IResult {
+        /// <summary>
+        /// Whether the operation succeeded.
+        /// </summary>
+        bool Success { get; }
+        /// <summary>
+        /// The result message.
+        /// </summary>
+        string Message { get; }
+        /// <summary>
+        /// The result error, if any.
+        /// </summary>
+        string Error { get; }
+    }
+}
---
+++ DinderWS/Models/Identity/IdentityLoginResult.cs
@@ -0,0 +1,18 @@
+﻿using DinderWS.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinderWS.Models.Identity {
+    public class IdentityLoginResult : IResult {
+        public bool Success { get; private set; }
+        public string Message { get; set; }
+        public string Error { get; set; }
+        public string Id { get; set; }
+
+        public IdentityLoginResult(bool success) {
+            Success = success;
+        }
+    }
+}
